bd618de1a0e5e0754b57a788b633209479f5a539
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2021-04-29 16:54:17 +0300

    Reupdated version

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -48,7 +48,7 @@ function App() {
   }, []);
 
   function handleCardLike(card) {
-    const isLiked=card.likes.some(item => item._id===currentUser._id);
+    const isLiked=card.likes.some(item => item===currentUser._id);
 
     api.changeLikeStatus(card._id, !isLiked)
       .then((newCard) => {
