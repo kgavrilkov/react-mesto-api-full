767e4f6835107fad6656abca8f44388a9d430994
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2021-04-30 15:22:06 +0300

    Redressed project

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -176,7 +176,7 @@ function App() {
   }
 
   const handleOverlayClick = (evt) => {
-    if (evt.target.classList.contains('popup_close') || evt.target.classList.contains('popup')) {
+    if (evt.target.classList.contains('popup_opened')) {
       closeAllPopups();
     }
   }
