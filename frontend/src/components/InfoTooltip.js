6bebf0eda9c52cef9bf02b04fdb4df19311298de
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2021-04-30 17:08:30 +0300

    Repaired project

--- frontend/src/components/InfoTooltip.js
+++ frontend/src/components/InfoTooltip.js
@@ -6,8 +6,25 @@ function InfoTooltip({isOpen, onClose, isRegistered}) {
   const infoTooltipPic=isRegistered ? successPicPath : failPicPath;
   const infoTooltipText=isRegistered ? "Вы успешно зарегистрировались!" : "Что-то пошло не так! Попробуйте ещё раз.";
 
+  React.useEffect(() => {
+    if (isOpen) {document.addEventListener("keydown", handleEscClose)}
+    return (() => {document.removeEventListener("keydown", handleEscClose)})
+  })
+
+  function handleEscClose(evt) {
+    if (evt.key==="Escape") {
+      onClose();
+    }
+  }
+
+  function onOverlayClose(evt) {
+    if (evt.target.classList.contains('popup_opened')) {
+      onClose();
+    }
+  }
+
   return (
-    <div className={`popup popup_type_tooltip ${isOpen && 'popup_opened'}`}>
+    <div className={`popup popup_type_tooltip ${isOpen && 'popup_opened'}`} onClick={onOverlayClose}>
       <div className="popup__container popup__container_type_tooltip">
         <div className="popup__form popup__form_type_tooltip">
           <img className="popup__image-tooltip" src={infoTooltipPic} alt="#" />
