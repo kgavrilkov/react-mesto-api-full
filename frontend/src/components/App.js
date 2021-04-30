a4fe57488a8d550c0456264546b21e2b3749f8e3
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2021-04-30 06:24:42 +0300

    Rectified version

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -130,7 +130,7 @@ function App() {
     });
   }
 
-  const tokenCheck = React.useCallback(() => {
+  const tokenCheck = () => {
     const token=localStorage.getItem('token');
     if (token) {
       auth.getContent(token)
@@ -145,11 +145,11 @@ function App() {
         })
         .catch(() => history.push('/signin'));
     }
-  }, [history])
+  }
 
   React.useEffect(() => {
     tokenCheck();
-  }, [tokenCheck, loggedIn]);
+  },[]);
 
   const handleSignOut = () => {
     localStorage.removeItem('token');
