6b438e8b58d53cc8ea5c3ae02d9bc87a88aa64b0
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2021-05-03 14:50:47 +0300

    Repaired project

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -45,7 +45,7 @@ const login = (req, res, next) => {
     .then((user) => {
       const token = jwt.sign(
         { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`,
         { expiresIn: '7d' },
       );
       res.send({ token });
--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,7 @@
 const jwt = require('jsonwebtoken');
 const UnauthorizedError = require('../errors/unauthorized-error');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const auth = (req, res, next) => {
   const { authorization } = req.headers;
@@ -12,7 +12,7 @@ const auth = (req, res, next) => {
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
-    payload = jwt.verify(token, JWT_SECRET);
+    payload = jwt.verify(token, `${NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'}`);
   } catch (err) {
     throw new UnauthorizedError('Необходима авторизация');
   }
